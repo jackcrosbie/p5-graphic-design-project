36a2dd4347476f300c0e54f0bd7b67e0656b77ee
jackcrosbie <jack.crosbie@mycit.ie>
2022-05-18 14:13:53 +0000

    added view for reviews app

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from .models import Review
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+
 def reviews(request):
     """ Return homepage """
     return render(request, 'reviews/reviews.html')
+
+
+class ReviewView(CreateView):
+    """ view for generating the contact form """
+    model = Review()
+    template_name = "reviews/reviews.html"
+    success_url = '/'
