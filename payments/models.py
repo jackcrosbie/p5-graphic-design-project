c4dd7f7657530832e458d4f7ae76bb92e4039e3f
jackcrosbie <jack.crosbie@mycit.ie>
2022-05-24 15:20:49 +0000

    updated payments model for orders and installed relevant packages

--- payments/models.py
+++ payments/models.py
@@ -1,6 +1,10 @@
 """ django imports """
-from django.db import models
 import uuid
+
+from django.db import models
+from django_countries.fields import CountryField
+
+from profiles.models import Profile
 
 
 project_choices = (
@@ -36,3 +40,27 @@ class Quotes(models.Model):
 
     def __str__(self):
         return self.quote_id
+
+
+class Order(models.Model):
+    """ editbale=false is used for a unqiue order id which can't be changed"""
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(Profile, on_delete=models.SET_NULL, 
+                                    null=True, blank=True, related_name='orders')
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
