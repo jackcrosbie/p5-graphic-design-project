945c1f440db31b4f09353b863eaadbacbbad645f
jackcrosbie <jack.crosbie@mycit.ie>
2022-05-27 21:37:55 +0000

    debugging reviews

--- reviews/models.py
+++ reviews/models.py
@@ -16,7 +16,7 @@ class UserReviews(models.Model):
     """ model for user reviews used to create and view reviews """
     title = models.CharField(max_length=50, blank=True)
     review = models.TextField(max_length=1000)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     rate = models.IntegerField(choices=RATINGS, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
