e79994b4e72371785139d6c5d4ac7ea83a8d43ee
jackcrosbie <jack.crosbie@mycit.ie>
2022-07-26 00:05:58 +0000

    updated settings.py to fix email verifications being sent

--- graphic_design/settings.py
+++ graphic_design/settings.py
@@ -100,7 +100,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
@@ -132,7 +131,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
